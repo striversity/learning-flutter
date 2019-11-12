d709968d51e71b4579615875efce8cdea9a28672
Verrol Adams <verrol@lorrev.org>
2019-11-12 08:12:37 -0500

    #04 - Flutter Basics - Part 6 - Xylophone App - Step 2

--- 04-flutter-basics/part6_xylophone/lib/main.dart
+++ 04-flutter-basics/part6_xylophone/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(MyApp());
+
+final AudioCache player = AudioCache();
 
 final keyColors = <Color>[
   Colors.green,
@@ -17,29 +20,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Center(
-        child: LayoutBuilder(
-          builder: (context, constraint) {
-            var maxWidth = constraint.maxWidth / (keyColors.length + 2);
+      child: LayoutBuilder(
+        builder: (context, constraint) {
+          var maxWidth = constraint.maxWidth / (keyColors.length + 2);
 
-            var w = maxWidth - (maxWidth * 0.3);
-            var h = 200.0;
-
-            final myKeys = keyColors
-                .map((c) => XylophoneKey(
-                      color: c,
-                      width: w,
-                      height: h,
-                    ))
-                .toList();
-
-            return Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              textDirection: TextDirection.ltr,
-              children: myKeys,
+          var w = maxWidth - (maxWidth * 0.3);
+          var h = 200.0;
+          var i = 5;
+          final myKeys = keyColors.map((c) {
+            ++i;
+            return XylophoneKey(
+              color: c,
+              width: w,
+              height: h,
+              sound: 'sample$i',
             );
-          },
-        ),
+          }).toList();
+
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            textDirection: TextDirection.ltr,
+            children: myKeys,
+          );
+        },
       ),
     );
   }
@@ -49,19 +52,22 @@ class XylophoneKey extends StatelessWidget {
   final Color color;
   final double width;
   final double height;
+  final String sound;
 
-  XylophoneKey({this.color, this.width, this.height});
+  XylophoneKey({this.color, this.width, this.height, this.sound});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
         print("I was tapped: ${color.toString()}");
+        player.play('sounds/$sound.mp3');
       },
       child: Container(
-        color: color,
         width: width,
         height: height,
+        decoration: BoxDecoration(
+            color: color, borderRadius: BorderRadius.circular(16.0)),
       ),
     );
   }
